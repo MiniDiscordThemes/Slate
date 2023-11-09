ba62920ce6fd8c0251bb706125fffdc57117fea8
Saltssaumure <Saltssaumure@users.noreply.github.com>
2023-11-09 18:35:02 +0000

    deploy: af0d9c1949b3aab41e4774f4d2dbd71b5233b05c

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "Slate" /* The official name of your theme. */,
+        scss: "slate" /* The name of your base scss file. */,
+        repo: "Slate" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
